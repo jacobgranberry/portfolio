fb7f4f5eddba08ff412683122d986444b1b74e70
Jacob Granberry <jacobgranberry@gmail.com>
2019-01-05 17:17:57 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0565888023b2283e3f3933990b6cf0d1.js"
+  "/portfolio/precache-manifest.e00b17264413a657f4d69d6e0071cc73.js"
 );
 
 workbox.clientsClaim();
