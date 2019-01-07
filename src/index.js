5c29e09faef1dd963c8550a43e37a166390aaf0e
Jacob Granberry <jacobgranberry@gmail.com>
2019-01-06 18:37:56 -0600

    revert router

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Router } from 'react-router';
-import createHashHistory from 'history/createHashHistory';
+import { BrowserRouter } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab, faLinkedinIn, faInstagram, faGithubAlt } from '@fortawesome/free-brands-svg-icons';
 import { fas, faCircle } from '@fortawesome/free-solid-svg-icons';
 import 'normalize.css';
 import './theme/globalStyle';
-import "animate.css/animate.min.css";
+import "animate.css/animate.min.css";;
 
 require('typeface-montserrat');
 require('typeface-righteous');
 
 library.add( fab, faLinkedinIn, faGithubAlt, faInstagram, fas, faCircle );
 
-const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
-
 ReactDOM.render(
-<Router history={hashHistory}>
+<BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-</Router>,
+</BrowserRouter>,
 document.getElementById('root')
 );
