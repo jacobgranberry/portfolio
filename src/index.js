eca3b41038bef86101f3a7225e95597b53e0d8c6
Jacob Granberry <jacobgranberry@gmail.com>
2019-01-06 18:33:03 -0600

    hash history

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router';
+import createHashHistory from 'history/createHashHistory';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab, faLinkedinIn, faInstagram, faGithubAlt } from '@fortawesome/free-brands-svg-icons';
 import { fas, faCircle } from '@fortawesome/free-solid-svg-icons';
@@ -14,9 +15,11 @@ require('typeface-righteous');
 
 library.add( fab, faLinkedinIn, faGithubAlt, faInstagram, fas, faCircle );
 
+const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
+
 ReactDOM.render(
-<BrowserRouter basename={process.env.PUBLIC_URL}>
+<Router history={hashHistory}>
     <App />
-</BrowserRouter>,
+</Router>,
 document.getElementById('root')
 );
